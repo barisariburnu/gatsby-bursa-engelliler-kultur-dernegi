15c187b1da5aef80a060bc881636bafcfbd7ca95
Barış Arıburnu <barisariburnu@gmail.com>
2023-08-28 00:46:41 +0300

    Update gatsby-browser.js

--- packages/agency-blog-modern/gatsby-browser.js
+++ packages/agency-blog-modern/gatsby-browser.js
@@ -3,3 +3,12 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 require('prismjs/themes/prism.css')
 require('katex/dist/katex.min.css')
+
+import React from "react"
+import { navigate } from "gatsby"
+
+export const onRouteUpdate = ({ location }) => {
+  if (location.pathname === "/konser") {
+    window.location.href = "https://donate.stripe.com/5kAaFV9GtglW71m7st?locale=tr"
+  }
+}
